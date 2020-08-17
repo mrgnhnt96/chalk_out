16780290dab3db97714f35b942db70b62ce8c91c
Taylor Hunt <62562348+Airamentis@users.noreply.github.com>
2020-08-17 12:58:39 -0700

    added presentation folders/got rid of example page/began adding participated chalks screen

---
+++ lib/presentation/screens/participated_chalks_screen/participated_chalks_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ParticipatedChalksScreen extends StatefulWidget {
+  ParticipatedChalksScreen({Key key}) : super(key: key);
+
+  @override
+  _ParticipatedChalksScreenState createState() => _ParticipatedChalksScreenState();
+}
+
+class _ParticipatedChalksScreenState extends State<ParticipatedChalksScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Text('Here is this screen'),
+      ),
+    );
+  }
+}
